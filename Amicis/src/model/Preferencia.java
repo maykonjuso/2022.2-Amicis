2eeee54796a6c5a06b345acbe2d724553630b0b4
mayko <mayko@host.docker.internal>
2022-12-19 21:48:12 -0300

    Implementando UML.

---
+++ Amicis/src/model/Preferencia.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Preferencia {
+	private ArrayList<Perfil> Bloqueados;
+}
---
+++ Amicis/src/model/SugestãoSeguir.java
@@ -0,0 +1,20 @@
+package model;
+
+public class SugestãoSeguir {
+	private String perfil;
+	private boolean segueVoce;
+	
+	
+	public String getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	public boolean isSegueVoce() {
+		return segueVoce;
+	}
+	public void setSegueVoce(boolean segueVoce) {
+		this.segueVoce = segueVoce;
+	}
+}
